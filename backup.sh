8685e8c0fac192c8a1e2f3414dca820be4f319c7
JMoitta <joaomoitamanrique@gmail.com>
2017-05-30 19:54:58 -0300

    Create backup.sh

---
+++ backup.sh
@@ -0,0 +1,13 @@
+export PGPASSWORD = "senha123"
+
+Data2=`date "+%Y%m%d-%H:%M"
+
+NOMEARQUIVO="backup.bd"
+
+pg_dump -U postgres -h localhost -O -o  -b -F c nomedobanco > $NOMEARQUIVO
+
+psql -U postgres -h localhost "drop database if exists bancoretorno;"
+
+psql -U postgres -h localhost "create database bancoretorno;"
+
+pg_restore -U postgres -h localhost -d bancoretorno $NOMEARQUIVO
